11b6c5ef697c3bd1d2c62e822832e398aec7a16d
epolekoff <epolekoff@gmail.com>
2020-07-11 04:56:37 -0700

    Built a little scene for the game.

---
+++ Assets/Scripts/NPC/NPC.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour
+{
+    public string SpeechText;
+    public SpeechBubble SpeechBubble;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Check if a player gets close.
+    /// </summary>
+    void OnTriggerEnter(Collider col)
+    {
+        PlayerCharacter pc = col.GetComponentInParent<PlayerCharacter>();
+        if (pc != null)
+        {
+            SpeechBubble.Show(true, SpeechText);
+        }
+    }
+
+    /// <summary>
+    /// When a player leaves.
+    /// </summary>
+    void OnTriggerExit(Collider col)
+    {
+        PlayerCharacter pc = col.GetComponentInParent<PlayerCharacter>();
+        if (pc != null)
+        {
+            SpeechBubble.Show(false, SpeechText);
+        }
+    }
+}
---
+++ Assets/Scripts/NPC/SpeechBubble.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeechBubble : MonoBehaviour
+{
+    public TMPro.TextMeshProUGUI Text;
+    public Animator Animator;
+
+    private bool m_isShowing = false;
+
+    private const float CharacterSpeed = 0.05f;
+
+    /// <summary>
+    /// Show or hide,
+    /// </summary>
+    public void Show(bool shown, string newText)
+    {
+        if(m_isShowing == shown)
+        {
+            return;
+        }
+
+        m_isShowing = shown;
+        StopAllCoroutines();
+
+        // Set the text.
+        if(shown)
+        {
+            StartCoroutine(SetTextOverTime(newText));
+        }
+
+        // Play animation to show or hide.
+        Animator.SetTrigger(shown ? "Show" : "Hide");
+    }
+
+    /// <summary>
+    /// Make the characters appear over time.
+    /// </summary>
+    private IEnumerator SetTextOverTime(string text)
+    {
+        Text.text = text;
+        yield return null;
+
+        int shownCharacters = 0;
+        int maxCharacters = Text.textInfo.characterCount;
+
+        while (shownCharacters <= maxCharacters)
+        {
+            Text.maxVisibleCharacters = shownCharacters;
+            shownCharacters++;
+            yield return new WaitForSeconds(CharacterSpeed);
+        }
+
+        Text.maxVisibleCharacters = maxCharacters;
+    }
+}
